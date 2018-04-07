f15bb5ec5087e5d874cb6ef94b92d965ce33e202
wanglei5205 <wanglei5205@126.com>
2018-04-07 15:51:33 +0800

    堆排序

--- sort/HeapSort.cpp
+++ sort/HeapSort.cpp
@@ -2,37 +2,50 @@
 using namespace std;
 int n;
 int a[105];
-//下标从1开始
+
+// 构建大根堆,其中a是第一个非叶子节点，z是节点个数
 void HeapAdjust(int A[], int a, int z)
 {
-    int i, j;
-    for(i = a, j = 2 * i; j <= z; i = j, j = 2 * i)
-    { //i为父，j为子
-        if(j < z && A[j + 1] > A[j]) j++;   //大顶堆，沿大孩子方向下行
+    // 重建最大堆，i是父节点，j是左子节点
+    for(int i = a, j = 2 * i; j <= z; i = j, j = 2 * i)
+    {
+        // 取i的左右子节点中较大的子节点
+        if(j < z && A[j + 1] > A[j])
+            j++;
+
         if(A[i] < A[j])
             swap(A[i], A[j]);
-        else break;
+        else
+            break;
     }
 }
+
+// 从右到做，从下到上
 void HeapSort(int A[], int n)
 {
-    int i;
-    for(i = n / 2; i >= 1; i--) //从倒数第一个非叶子结点，自下而上堆化
+    // 第一步：建立大根堆（i是第一个非叶子节点，n是节点个数）
+    for(int i = n / 2; i >= 1; i--)
+    {
         HeapAdjust(A, i, n);
-    for(i = n; i > 1; i--)
-    { //交换A[1]与最后元素A[i]（i=n, ..., 1）, 再堆化
+    }
+
+    for(int i = n; i >= 1; i--)
+    {
+        // 第二步：交换堆元素（堆首和堆尾）
         swap(A[1], A[i]);
+
+        // 第三步：重建大根堆
         HeapAdjust(A, 1, i - 1);
     }
 }
 int main()
 {
-    int i;
     cin >> n;
-    for(i = 1; i <= n; i++)
+    for(int i = 1; i <= n; i++)
     cin >> a[i];
     HeapSort(a, n);
-    for(i = 1; i <= n; i++) cout << a[i] << " ";
+    for(int i = 1; i <= n; i++) cout << a[i] << " ";
     cout << endl;
+    cout<<a[0]<<endl;
     return 0;
 }
