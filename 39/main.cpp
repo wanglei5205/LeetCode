04016f463a3ab46f9dd158b288c59c71f50efb4f
wanglei5205 <wanglei5205@126.com>
2018-04-13 15:53:08 +0800

    39题：数组中出现次数超过数组长度一半的数字

---
+++ 39/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int fun(vector<int> num)
+{
+    // 鲁棒性
+    if(num.size() == 0)
+        return 0;
+
+    // 查找
+    int res = num[0];
+    int cnt = 1;
+
+    for(int i=1;i<num.size();++i)
+    {
+        if(num[i]==res)
+            ++cnt;
+        else
+            --cnt;
+
+        if(cnt == 0)
+        {
+            res = num[i];
+            ++cnt;
+        }
+    }
+
+    // 验证
+    cnt = 0;
+    for(int i = 0;i<num.size();++i)
+        if(res == num[i])
+            ++cnt;
+    cout<<cnt<<endl;
+    if(cnt*2 > num.size())
+        return res;
+    else
+        return 0;
+}
+int main()
+{
+    vector<int> num ={1,2,3,3,2,2,2};
+    cout<<fun(num)<<endl;
+    return 0;
+}
