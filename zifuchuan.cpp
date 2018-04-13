2b4671053258a132569783b6a613de822f5b64db
wanglei5205 <wanglei5205@126.com>
2018-04-13 15:52:32 +0800

    39题数组中出现次数超过数组长度一半的数字

---
+++ zifuchuan.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<string> Permutation(string str) {
+    // 存储结果
+    vector<string> result;
+    // 边界条件
+    if(str.size()==0) return result;
+    // 递归实现字符串的字典序全排列
+    Permutation(result, str, 0);
+    // 调整顺序
+    sort(result.begin(), result.end());
+    // 返回结果
+    return result;
+    }
+
+void Permutation(vector<string> &result, string str, int begin)
+{
+    // 递归出口
+    if(begin==str.size()-1) result.push_back(str);
+
+    for(int i=begin; i<=str.size()-1;++i)
+    {
+        // 去重
+        if(i!=begin && str[i]==str[begin]) continue;
+
+        // 全排列
+        swap(str[i], str[begin]);
+        Permutation(result, str, begin+1);
+        swap(str[i], str[begin]);
+    }
+}
+
+int main()
+{
+    string str="abc";
+    Permutation(str);
+}
+
