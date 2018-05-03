c9292abaefdf94f573df3e2c1b581a7c399e176f
wanglei5205 <heuwl1314520wl>
2018-05-03 10:56:39 +0800

    58B题：坐旋转字符串

--- 剑指offer/58A/main.cpp
+++ 剑指offer/58A/main.cpp
@@ -1,9 +1,68 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
+class Solution {
+public:
+    string ReverseSentence(string str)
+    {
+        // 特殊输入
+        int len=str.length();
+        if(len==0) return "";
+
+        // 使用翻转函数翻转整个句子
+        Reverse(str,0,len-1);
+        cout<<"句子翻转"<<endl;
+        cout<<str<<endl;
+
+        // 寻找单词并使用翻转内函数翻转单词
+        int i=0;//辅助变量
+        int j=0;//辅助变量
+
+        while(i<len)
+        {
+            // 寻找单词开头
+            if(str[i]==' ')
+            {
+                i++;
+                j++;
+            }
+
+            // 寻找单词结尾
+            if(str[j]!=' ' && str[j]!='\0')
+            {
+                j++;
+            }
+            else
+            {
+                Reverse(str,i,--j);
+                i=++j;
+            }
+            cout<<"i="<<i<<endl;
+            cout<<"j="<<j<<endl;
+            cout<<str<<endl;
+        }
+        return str;
+    }
+
+    // 翻转函数
+    void Reverse(string &str,int pstart,int pend)
+    {
+        while(pstart < pend)
+        {
+            char temp=str[pstart];
+            str[pstart]=str[pend];
+            str[pend]=temp;
+
+            pstart++;
+            pend--;
+        }
+    }
+};
 int main()
 {
-    cout << "Hello world!" << endl;
+    string str = "I am a student.";
+    Solution solution;
+    solution.ReverseSentence(str);
     return 0;
 }
--- 剑指offer/58B/main.cpp
+++ 剑指offer/58B/main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+
+class Solution {
+public:
+    string LeftRotateString(string &str, int n)
+    {
+        int len = str.size();
+        // 特殊输入
+        if(!str.empty() && n <= len && n >= 0)
+        {
+
+            int pFirstStart = 0;
+            int pFirstEnd = n - 1;
+            int pSecondStart = n;
+            int pSecondEnd = len - 1;
+
+            // 翻转字符串的前面n个字符
+            reverse(str, pFirstStart, pFirstEnd);
+            // 翻转字符串的后面部分
+            reverse(str, pSecondStart, pSecondEnd);
+            // 翻转整个字符串
+            reverse(str, pFirstStart, pSecondEnd);
+
+        }
+        return str;
+    }
+
+    // 翻转函数
+    void reverse(string &str, int begin, int end)
+    {
+        while(begin < end)
+        {
+            char tmp = str[begin];
+            str[begin] = str[end];
+            str[end] = tmp;
+            begin++;
+            end--;
+        }
+    }
+};
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    // 测试用例
+    string str = "abcdefg";
+    int n = 2;
+
+    // 函数调用
+    Solution solution;
+    solution.LeftRotateString(str,n);
+    cout<<str<<endl;
     return 0;
 }
