623c5b34018ea9520216f8f7953cf4610cb5624a
wanglei5205 <heuwl1314520wl>
2018-05-10 08:09:28 +0800

    圆圈中最后剩下的数字

---
+++ 剑指offer/62/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    // n表示多少个人，m表示随机数
+    int LastRemaining_Solution(int n, int m)
+    {
+        // 特殊输入
+        if(n == 0 || m < 0) return -1;
+
+        // 递推公式计算
+        int res = 0;
+        for(int i = 2; i <= n; i++)
+        {
+            res = (res + m)%i;
+            cout<<res<<endl;
+        }
+        return res;
+    }
+};
+int main()
+{
+    int n = 11;
+    int m = 3;
+    Solution solution;
+    solution.LastRemaining_Solution(n,m);
+    return 0;
+}
